31d49ff36bcc376b90eed5b71d1e8e9f95fc5382
yusrilihzaM <mohyusrilihzamaulana@gmail.com>
2021-04-03 18:29:57 +0700

    Initial commit

---
+++ app/src/main/java/com/yusril/codelabpreloaddata/MahasiswaActivity.kt
@@ -0,0 +1,11 @@
+package com.yusril.codelabpreloaddata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MahasiswaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mahasiswa)
+    }
+}
---
+++ app/src/main/java/com/yusril/codelabpreloaddata/database/DatabaseContract.kt
@@ -0,0 +1,13 @@
+package com.yusril.codelabpreloaddata.database
+
+import android.provider.BaseColumns
+
+internal object DatabaseContract {
+    var TABLE_NAME = "table_mahasiswa"
+    internal class MahasiswaColumns : BaseColumns {
+        companion object {
+            const val NAMA = "nama"
+            const val NIM = "nim"
+        }
+    }
+}
